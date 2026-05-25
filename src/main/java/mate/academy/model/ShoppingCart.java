6c6b859715a540d283f410ef8efa74d80330bc1c
Denya Basarab <denya2007basarab@gmail.com>
2026-05-25 16:54:58 +0300

    feat: add models

---
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -0,0 +1,52 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.List;
+
+@Entity
+public class ShoppingCart {
+    @Id
+    private Long id;
+    @OneToMany
+    @JoinColumn(name = "tickets")
+    private List<Ticket> ticket;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(List<Ticket> ticket) {
+        this.ticket = ticket;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" + "id=" + id + ", ticket="
+                + ticket + ", user=" + user + '}';
+    }
+}
